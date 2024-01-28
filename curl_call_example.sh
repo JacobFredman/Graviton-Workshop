9a36128c7e67ce0f56882a96d42946049168a1ab
“Jacob <jacov141@gmail.com>
2024-01-28 10:24:27 +0200

    wrk test

--- curl_call_example.sh
+++ curl_call_example.sh
@@ -1,6 +1,9 @@
-// working fine as post from curl
+# working fine as post from curl
  curl -X POST -H "Content-Type: application/json" -d '{"url":"http://example111.com"}' http://localhost:5000/short_url
 {"short_url":"jhcyrjoqre"}
 
-// working fine as get from curl
+# working fine as get from curl
 curl -X GET http://184.73.141.145:5000/get_full_url?short_url=jerluhvkxi
+
+# run wrk load test
+./wrk -c10 -t30 -d 1m -L -R 100 -s ./post.lua http://184.73.141.145:5000/short_url
